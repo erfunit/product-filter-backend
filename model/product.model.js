0d0d196746eab63777421ab61908658e6e39bf2f
Erfun Ghodoosi <erfunghodoosi@gmail.com>
2023-08-14 16:49:30 +0330

    fucked up

--- model/product.model.js
+++ model/product.model.js
@@ -1,189 +1,23 @@
-const mysql = require("mysql2");
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+const { DataTypes } = require("sequelize");
+const db = require("../utils/database"); // Create this file to configure database connection
+
+const Product = db.define("product", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  tags: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
 });
 
-const addNewProduct = async (req, res) => {
-  try {
-    const { name, price, tags, description } = req.body;
-
-    if (!name || !price || !tags || !description) {
-      return res
-        .status(400)
-        .json({ error: "To create a new product, please fill all the fields" });
-    }
-
-    const newProduct = {
-      name,
-      price,
-      tags: JSON.stringify(tags),
-      description,
-    };
-
-    pool.query(
-      "INSERT INTO products (name, price, tags, description) VALUES (?, ?, ?, ?)",
-      [
-        newProduct.name,
-        newProduct.price,
-        newProduct.tags,
-        newProduct.description,
-      ],
-      (err, results) => {
-        if (err) {
-          console.error("Error adding new product:", err);
-          return res.status(500).json({ message: "Internal server error" });
-        }
-        const insertedProductId = results.insertId;
-        res.status(201).json({
-          message: "New product added successfully!",
-          data: { id: insertedProductId, ...newProduct },
-        });
-      }
-    );
-  } catch (error) {
-    console.error("Error adding new product:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
-
-const getProducts = async (req, res) => {
-  try {
-    pool.query("SELECT * FROM products", (err, results) => {
-      if (err) {
-        console.error("Error fetching products:", err);
-        return res.status(500).json({ message: "Internal server error" });
-      }
-      res.json(results);
-    });
-  } catch (error) {
-    console.error("Error fetching products:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
-
-const showOneProduct = async (req, res) => {
-  try {
-    const productId = req.params.id;
-
-    pool.query(
-      "SELECT * FROM products WHERE id = ?",
-      [productId],
-      (err, results) => {
-        if (err) {
-          console.error("Error fetching product:", err);
-          return res.status(500).json({ message: "Internal server error" });
-        }
-        if (results.length === 0) {
-          return res.status(404).json({ message: "Product not found" });
-        }
-
-        const product = results[0];
-        res.json({ data: product });
-      }
-    );
-  } catch (error) {
-    console.error("Error fetching product:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
-
-const deleteProduct = async (req, res) => {
-  try {
-    const productId = req.params.id;
-
-    pool.query(
-      "SELECT * FROM products WHERE id = ?",
-      [productId],
-      (err, results) => {
-        if (err) {
-          console.error("Error fetching product:", err);
-          return res.status(500).json({ message: "Internal server error" });
-        }
-        if (results.length === 0) {
-          return res.status(404).json({ message: "Product not found" });
-        }
-
-        const product = results[0];
-
-        pool.query(
-          "DELETE FROM products WHERE id = ?",
-          [productId],
-          (err, results) => {
-            if (err) {
-              console.error("Error deleting product:", err);
-              return res.status(500).json({ message: "Internal server error" });
-            }
-            res.json({
-              message: "Product deleted successfully!",
-              data: product,
-            });
-          }
-        );
-      }
-    );
-  } catch (error) {
-    console.error("Error deleting product:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
-
-const updateProduct = async (req, res) => {
-  try {
-    const productId = req.params.id;
-    const updatedData = req.body;
-
-    pool.query(
-      "SELECT * FROM products WHERE id = ?",
-      [productId],
-      (err, results) => {
-        if (err) {
-          console.error("Error fetching product:", err);
-          return res.status(500).json({ message: "Internal server error" });
-        }
-        if (results.length === 0) {
-          return res.status(404).json({ message: "Product not found" });
-        }
-
-        const existingProduct = results[0];
-
-        pool.query(
-          "UPDATE products SET name = ?, price = ?, tags = ?, description = ? WHERE id = ?",
-          [
-            updatedData.name,
-            updatedData.price,
-            JSON.stringify(updatedData.tags),
-            updatedData.description,
-            productId,
-          ],
-          (err, results) => {
-            if (err) {
-              console.error("Error updating product:", err);
-              return res.status(500).json({ message: "Internal server error" });
-            }
-            res.json({
-              message: "Product updated successfully!",
-              data: { id: productId, ...updatedData },
-            });
-          }
-        );
-      }
-    );
-  } catch (error) {
-    console.error("Error updating product:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
-
-module.exports = {
-  addNewProduct,
-  getProducts,
-  deleteProduct,
-  showOneProduct,
-  updateProduct,
-};
+module.exports = Product;
--- utils/connect.js
+++ utils/connect.js
@@ -1,27 +1,23 @@
-const mysql = require("mysql2");
-const dotenv = require("dotenv");
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-dotenv.config({ path: ".env" });
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
-
-const connect = () => {
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error("Error connecting to database:", err);
-      return;
-    }
+const connect = async () => {
+  try {
+    await sequelize.authenticate();
     console.log("Connected to database!");
-    connection.release();
-  });
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+  }
 };
 
-module.exports = { connect };
+module.exports = { sequelize, connect };
